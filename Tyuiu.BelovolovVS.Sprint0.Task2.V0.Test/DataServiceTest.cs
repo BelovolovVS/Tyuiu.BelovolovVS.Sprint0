872ef1d10b55c299b13d89f696cf2764abaa239d
Вячеслав Беловолов <samuraiget2008@icloud.com>
2026-09-10 23:06:25 +0500

    Task2

---
+++ Tyuiu.BelovolovVS.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿namespace Tyuiu.BelovolovVS.Sprint0.Task2.V0.Test
+{
+    using Tyuiu.BelovolovVS.Sprint0.Task2.V0.Lib;
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Slava";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет Slava", res);
+        }
+    }
+}
